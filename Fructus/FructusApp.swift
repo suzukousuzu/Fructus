de7579920c2f2b9bbbe3d30fd2cf354712251fd4
Kota Suzuki <addressuzuki@gmail.com>
2024-02-11 11:48:39 +0900

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by 鈴木航太 on 2024/02/11.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
